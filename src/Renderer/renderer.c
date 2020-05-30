20ce9ce2c758a0d02f5338341907095f1b22c91d
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-05-30 09:09:42 +0100

    Begin implementing ability to load textures

--- src/Renderer/renderer.c
+++ src/Renderer/renderer.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
+#include <stdbool.h>
 #include "renderer.h"
 
 struct td_renderer {
     SDL_Window *window;
     SDL_Renderer *renderer;
+};
+
+struct td_renderable {
+    SDL_Texture *texture;
 };
 
 /*
@@ -63,6 +68,48 @@ td_renderer initRenderer(int width, int height) {
     renderer -> renderer = sdlRenderer;
 
     return renderer;
+}
+/*
+Private helper function to load textures from a path
+*/
+SDL_Texture *loadTexture(const char *path, td_renderer renderer){
+    //The final texture
+    SDL_Texture *newTexture = NULL;
+    //Checks if something went wrong
+    bool failure = false;
+
+    //Load image at specified path
+    SDL_Surface *loadedSurface = IMG_Load( path );
+    if( loadedSurface == NULL )
+    {
+        fprintf(stderr, "Unable to load image %s! SDL_image Error: %s\n", path, IMG_GetError() );
+        failure = true;
+    }
+    else
+    {
+        //Create texture from surface pixels
+        newTexture = SDL_CreateTextureFromSurface( renderer -> renderer, loadedSurface );
+        if( newTexture == NULL )
+        {
+            fprintf(stderr, "Unable to create texture from %s! SDL Error: %s\n", path, SDL_GetError() );
+            failure = true;
+        }
+        //Get rid of old loaded surface
+        SDL_FreeSurface( loadedSurface );
+    }
+
+    if(failure) return NULL;
+    else return newTexture;
+}
+
+td_renderable createRendereable(const char *path, td_renderer renderer) {
+    td_renderable renderable = malloc(sizeof(struct td_renderable));
+
+    renderable -> texture = loadTexture(path, renderer);
+
+    //TODO: Register the renderable with the renderer
+
+    return renderable;
 }
 
 void renderFrame(td_renderer renderer) {
--- src/Renderer/renderer.h
+++ src/Renderer/renderer.h
@@ -3,6 +3,8 @@
 
 typedef struct td_renderer *td_renderer;
 
+typedef struct td_renderable *td_renderable;
+
 td_renderer initRenderer(int,int);
 
 void renderFrame(td_renderer);
