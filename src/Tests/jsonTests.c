35dabee03374136b2e7b780ad30ae341bc07f5ab
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-06-03 18:28:47 +0100

    Begin writing JSON tests

--- src/Tests/jsonTests.c
+++ src/Tests/jsonTests.c
@@ -1,5 +1,20 @@
+#include <stdio.h>
 #include "jsonTests.h"
+#include "testHelper.h"
+#include "../JSON/jsonParser.h"
+
+#define TEST_JSON "{\
+                        \"block_1\": {\
+                            \"value_1\": 42\
+                        }\
+                    }"
 
 int runJsonTests() {
+    td_json json = jsonParse(TEST_JSON);
+    int failedTests = 0;
+
+    // Get an int value from JSON
+    failedTests += assert(42, getJSONInt(json, "block_1.value_1", NULL), "getJSONInt no error");
+
     return 0;
 }
