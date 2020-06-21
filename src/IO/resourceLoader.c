1569dbf86a56f290272deacca40c649180fd0aae
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-06-21 19:07:29 +0100

    Define API for resource loader

--- src/IO/resourceLoader.c
+++ src/IO/resourceLoader.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
 #include "resourceLoader.h"
 #include "../DataStructures/HashMap/hashMap.h"
 
@@ -5,3 +8,19 @@ struct td_resourceLoader {
     td_hashMap resources;
     char *basePath;
 };
+
+td_resourceLoader createResourceLoader(char *basePath) {
+    return NULL;
+}
+
+char *loadPlaintext(td_resourceLoader rl, char *path) {
+    return NULL;
+}
+
+SDL_Texture *loadTexture(td_resourceLoader rl, char *path) {
+    
+}
+
+void destroyResourceLoader(td_resourceLoader rl) {
+
+}
--- src/IO/resourceLoader.h
+++ src/IO/resourceLoader.h
@@ -3,4 +3,12 @@
 
 typedef struct td_resourceLoader *td_resourceLoader;
 
+td_resourceLoader createResourceLoader(char *);
+
+char *loadPlaintext(td_resourceLoader, char*);
+
+SDL_Texture *loadTexture(td_resourceLoader, char *);
+
+void destroyResourceLoader(td_resourceLoader);
+
 #endif
