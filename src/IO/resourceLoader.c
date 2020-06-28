08cd85a3fb612e7d9ae558c949c47f70fb25797f
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-06-28 17:42:49 +0100

    Define creation method for resource loader. Allow for chaning of loader methods

--- src/IO/resourceLoader.c
+++ src/IO/resourceLoader.c
@@ -7,12 +7,27 @@
 struct td_resourceLoader {
     td_hashMap resources;
     char *basePath;
-    void (*plaintextLoader)(char *path);
-    void (*surfaceLoader)(char *path);
+    char *(*plaintextLoader)(char *path);
+    SDL_Surface *(*surfaceLoader)(char *path);
 };
 
-td_resourceLoader createResourceLoader(char *basePath) {
+char *plaintextLoader(char *path) {
     return NULL;
+}
+
+SDL_Surface *surfaceLoader(char *path) {
+    return NULL;
+}
+
+td_resourceLoader createResourceLoader(char *basePath) {
+    td_resourceLoader resourceLoader = malloc(sizeof(struct td_resourceLoader));
+
+    resourceLoader -> resources = createHashMap(30);
+    resourceLoader -> basePath = basePath;
+    resourceLoader -> plaintextLoader = plaintextLoader;
+    resourceLoader -> surfaceLoader = surfaceLoader;
+
+    return resourceLoader;
 }
 
 char *loadPlaintextResource(td_resourceLoader rl, char *path) {
@@ -21,6 +36,19 @@ char *loadPlaintextResource(td_resourceLoader rl, char *path) {
 
 SDL_Surface *loadSurfaceResource(td_resourceLoader rl, char *path) {
     return NULL;
+}
+
+void setPlaintextLoader(td_resourceLoader rl, char *(*loader)(char *)) {
+    rl -> plaintextLoader = loader;
+}
+
+void setSurfaceLoader(td_resourceLoader rl, SDL_Surface *(*loader)(char *)) {
+    rl -> surfaceLoader = loader;
+}
+
+void resetLoaders(td_resourceLoader rl) {
+    rl -> plaintextLoader = plaintextLoader;
+    rl -> surfaceLoader = surfaceLoader;
 }
 
 void destroyResourceLoader(td_resourceLoader rl) {
--- src/IO/resourceLoader.h
+++ src/IO/resourceLoader.h
@@ -9,6 +9,12 @@ char *loadPlaintextResource(td_resourceLoader, char*);
 
 SDL_Surface *loadSurfaceResource(td_resourceLoader, char *);
 
+void setPlaintextLoader(td_resourceLoader, char *(*loader)(char *));
+
+void setSurfaceLoader(td_resourceLoader, SDL_Surface *(*loader)(char *));
+
+void resetLoaders(td_resourceLoader);
+
 void destroyResourceLoader(td_resourceLoader);
 
 #endif
