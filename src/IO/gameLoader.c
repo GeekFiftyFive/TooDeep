4dd2bf47324d55588aa26ca80b15afd97c9892bc
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-07-06 13:42:33 +0100

    Begin implementing constructor and destructor for game object

--- src/IO/gameLoader.c
+++ src/IO/gameLoader.c
@@ -1,11 +1,29 @@
 #include <stdlib.h>
 #include "gameLoader.h"
 #include "resourceLoader.h"
+#include "../DataStructures/HashMap/hashMap.h"
+#include "../JSON/jsonParser.h"
 
 struct td_game {
     td_resourceLoader loader;
+    td_hashMap scenes;
+    td_json config;
+    char *title;
 };
 
 td_game loadGameFromDirectory(char *path) {
-    return NULL;
+    td_game game = malloc(sizeof(struct td_game));
+
+    game -> loader = createResourceLoader(path);
+    game -> scenes = createHashMap(10);
+
+    return game;
+}
+
+void destroyGame(td_game game) {
+    destroyResourceLoader(game -> loader);
+    destroyHashMap(game -> scenes);
+    freeJson(game -> config);
+    free(game -> title);
+    free(game);
 }
