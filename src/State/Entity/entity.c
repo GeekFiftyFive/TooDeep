77a53ff87006bda4d11a6cddb14a2fa721cf2053
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-12-11 18:46:22 +0000

    Add missing files

---
+++ src/State/Entity/entity.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "./entity.h"
+
+struct td_entity {
+
+};
+
+td_entity createEntity() {
+    return NULL;
+}
+
+void destroyEntity(td_entity entity) {
+    // TODO: Implement
+}
---
+++ src/State/Entity/entity.h
@@ -0,0 +1,10 @@
+#ifndef ENTITY
+#define ENTITY
+
+typedef struct td_entity *td_entity;
+
+td_entity createEntity();
+
+void destroyEntity(td_entity);
+
+#endif
