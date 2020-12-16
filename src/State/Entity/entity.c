ef46db8315f1e73f7bd128b7339af7f36e4cc1c0
GeekFiftyFive <op15736@my.bristol.ac.uk>
2020-12-16 18:22:45 +0000

    Implement create entity

--- src/State/Entity/entity.c
+++ src/State/Entity/entity.c
@@ -8,7 +8,11 @@ struct td_entity {
 };
 
 td_entity createEntity(td_renderable renderable, td_tuple position, td_tuple velocity) {
-    return NULL;
+    td_entity entity = malloc(sizeof(struct td_entity));
+    entity -> renderable = renderable;
+    entity -> position = position;
+    entity -> velocity = velocity;
+    return entity;
 }
 
 void destroyEntity(td_entity entity) {
