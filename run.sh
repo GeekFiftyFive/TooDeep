bf960531e05e18809d2729c114036ca3ddbe8484
GeekFiftyFive <op15736@my.bristol.ac.uk>
2021-01-02 10:56:58 +0000

    Don't use nproc when on macOS

--- run.sh
+++ run.sh
@@ -3,7 +3,12 @@ set -e
 echo Setting Up
 mkdir -p obj
 echo Compiling
-make -j$(nproc)
+OS=`uname -s`
+if [ "$OS" = "Darwin" ]; then
+    make -j$(sysctl -n hw.ncpu)
+else
+    make -j$(nproc)
+fi
 echo Running unit tests
 ./tooDeep -run-tests
 echo Launching game
