da3ad89ea12b1013395f0032af9896147ee74524
GeekFiftyFive <op15736@my.bristol.ac.uk>
2021-01-19 19:40:01 +0000

    Improve controls in example game

--- examples/spaceship/scripts/move.lua
+++ examples/spaceship/scripts/move.lua
@@ -1,3 +1,12 @@
 local entity = getEntity(entityID)
 local velocity = getEntityVelocity(entity)
-setEntityVelocity(entity, x, velocity["y"])
+
+if(velocity["x"] > 0.0 and direction == "right" or velocity["x"] < 0.0 and direction == "left")
+then
+    setEntityVelocity(entity, x, velocity["y"])
+end
+
+if(x > 0.0 or -1 * x > 0.0)
+then
+    setEntityVelocity(entity, x, velocity["y"])
+end
