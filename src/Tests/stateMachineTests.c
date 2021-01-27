0abac9e51ec52506b06e61444548aa422139a2e8
GeekFiftyFive <op15736@my.bristol.ac.uk>
2021-01-27 20:58:12 +0000

    Add additional test

--- src/Tests/stateMachineTests.c
+++ src/Tests/stateMachineTests.c
@@ -10,7 +10,9 @@ int runStateMachineTests() {
     td_stateMachineNode start = createStateMachineNode(machine, "start", true);
     td_stateMachineNode end = createStateMachineNode(machine, "end", false);
     td_stateMachineConnection connection = addStateMachineConnection(start, end);
+    td_stateMachineConnection connection2 = addStateMachineConnection(end, start);
     addStateMachineIntCondition(connection, "count", 5, TD_GT);
+    addStateMachineIntCondition(connection2, "count", 1, TD_EQ);
 
     for(int i = 0; i <= 5; i++) {
         updateStateMachineIntCondition(machine, "count", i);
@@ -21,6 +23,10 @@ int runStateMachineTests() {
     updateStateMachineIntCondition(machine, "count", 6);
     char *state = getCurrentStateId(machine);
     assertString("end", state, "State machine condition met");
+
+    updateStateMachineIntCondition(machine, "count", 1);
+    state = getCurrentStateId(machine);
+    assertString("start", state, "State machine condition met");
 
     destroyStateMachine(machine);
     return 0;
